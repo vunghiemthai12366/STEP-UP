f923be3637fabba0165ace3c77627a47596dfe64
vunghiemthai12366 <157681358+vunghiemthai12366@users.noreply.github.com>
2024-01-25 14:19:03 +0700

    Initial commit

---
+++ sum.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+//int sum_main() {
+ int main() {
+ 	int a, b, sum;
+ 	printf("Enter the first number: ");
+ 	scanf("%d", &a);
+ 	printf("Enter the second number: ");
+ 	scanf("%d", &b);
+ 	sum = a + b;
+ 	printf("The sun is: %d", sum);
+ 	return 0;
+}
